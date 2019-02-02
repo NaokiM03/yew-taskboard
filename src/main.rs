1a000d738f4fe06318227a9dc56f360a43ee6002
NaokiM03 <mattsun.phone@gmail.com>
2019-02-02 23:25:42 +0900

    yew Hello, world!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#[macro_use]
+extern crate yew;
+use yew::prelude::*;
+ 
+struct Model {
+}
+ 
+impl Component for Model {
+    type Message = ();
+    type Properties = ();
+ 
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Model { }
+    }
+ 
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        false
+    }
+}
+ 
+impl Renderable<Model> for Model {
+    fn view(&self) -> Html<Self> {
+        html! {
+            <div>{ "Hello, world!" }</div>
+        }
+    }
+}
+ 
 fn main() {
-    println!("Hello, world!");
+    yew::initialize();
+    App::<Model>::new().mount_to_body();
+    yew::run_loop();
 }
