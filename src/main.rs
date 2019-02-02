e022e9c1ccf0fc2c483bdf546f4046ef1112bf54
NaokiM03 <mattsun.phone@gmail.com>
2019-02-03 00:10:09 +0900

    add taskcard

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,18 @@ extern crate yew;
 use yew::prelude::*;
 
 struct Model {
+    state: State,
+}
+
+struct State {
+    tasks: Vec<Task>,
+}
+
+struct Task {
+    name: String,
+    assignee: String,
+    mandays: u32,
+    status: u32,
 }
 
 impl Component for Model {
@@ -10,7 +22,16 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Model { }
+        Model {
+            state: State {
+                tasks: vec! [
+                    Task { name: "Task 1".to_string(), assignee: "🐱".to_string(), mandays: 3, status: 1 },
+                    Task { name: "Task 2".to_string(), assignee: "🐶".to_string(), mandays: 2, status: 1 },
+                    Task { name: "Task 3".to_string(), assignee: "🐱".to_string(), mandays: 1, status: 2 },
+                    Task { name: "Task 4".to_string(), assignee: "🐹".to_string(), mandays: 3, status: 3 },
+                ]
+            }
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
@@ -18,14 +39,37 @@ impl Component for Model {
     }
 }
 
-fn view_column(status: u32, status_text: &str) -> Html<Model> {
+fn view_column(status: u32, status_text: &str, tasks: &Vec<Task>) -> Html<Model> {
     html! {
         <div class=format!("column status-{}", status),>
             <div class="tags has-addons",>
                 <span class="tag",>{ status_text }</span>
-                <span class="tag is-dark",>{ 0 }</span>
+                <span class="tag is-dark",>{ tasks.iter().filter(|e| e.status == status).count() }</span>
             </div>
+            { for tasks.iter().enumerate().filter(|e| e.1.status == status).map(view_task) }
         </div>
+    }
+}
+
+fn view_task((idx, task): (usize, &Task)) -> Html<Model> {
+    html! {
+        <div class="card",>
+            <div class="card-content",>
+                { &task.name }
+            </div>
+            <footer class="card-footer",>
+                <div class="card-footer-item",>
+                    { &task.assignee }
+                </div>
+                <div class="card-footer-item",>
+                    { format!("{} 人日", &task.mandays) }
+                </div>
+            </footer>
+            <footer class="card-footer",>
+              <a class="card-footer-item",>{ "◀︎" }</a>
+              <a class="card-footer-item",>{ "▶" }</a>
+            </footer>
+          </div>
     }
 }
 
@@ -35,9 +79,9 @@ impl Renderable<Model> for Model {
             <section class="section", id="board",>
                 <div class="container",>
                     <div class="columns",>
-                        { view_column(1, "未対応") }
-                        { view_column(2, "処理中") }
-                        { view_column(3, "完了") }
+                        { view_column(1, "未対応", &self.state.tasks) }
+                        { view_column(2, "処理中", &self.state.tasks) }
+                        { view_column(3, "完了", &self.state.tasks) }
                     </div>
                 </div>
              </section>
