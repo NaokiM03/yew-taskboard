1b1b1ab7643ee2ac316d9a763dc7d78b8134d2b4
NaokiM03 <mattsun.phone@gmail.com>
2019-02-02 23:41:56 +0900

    add taskboard base
    remove space

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 #[macro_use]
 extern crate yew;
 use yew::prelude::*;
- 
+
 struct Model {
 }
- 
+
 impl Component for Model {
     type Message = ();
     type Properties = ();
- 
+
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
         Model { }
     }
- 
+
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         false
     }
@@ -21,7 +21,18 @@ impl Component for Model {
 impl Renderable<Model> for Model {
     fn view(&self) -> Html<Self> {
         html! {
-            <div>{ "Hello, world!" }</div>
+            <section class="section", id="board",>
+                <div class="container",>
+                    <div class="columns",>
+                        <div class="column status-1",>
+                            <div class="tags has-addons",>
+                                <span class="tag",>{ "未処理" }</span>
+                                <span class="tag is-dark",>{ 0 } </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+             </section>
         }
     }
 }
